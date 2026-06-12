291f235dc06be8ff2ebc32579b40c7a4f8906a89
vuthdev <serey7vuth@gmail>
2026-06-12 16:37:57 +0700

    feat: add config for ghost persistent data

---
+++ src/main/kotlin/org/firestorm/deathproRemake/common/constants/GhostKeys.kt
@@ -0,0 +1,29 @@
+package org.firestorm.deathproRemake.common.constants
+
+import org.bukkit.NamespacedKey
+import org.bukkit.plugin.java.JavaPlugin
+import org.firestorm.deathproRemake.DeathproRemake
+
+object GhostKeys {
+    lateinit var EXPIRE_AT: NamespacedKey private set
+    lateinit var DEATH_WORLD: NamespacedKey private set
+    lateinit var DEATH_X: NamespacedKey private set
+    lateinit var DEATH_Y: NamespacedKey private set
+    lateinit var DEATH_Z: NamespacedKey private set
+    lateinit var RESPAWN_WORLD: NamespacedKey private set
+    lateinit var RESPAWN_X: NamespacedKey private set
+    lateinit var RESPAWN_Y: NamespacedKey private set
+    lateinit var RESPAWN_Z: NamespacedKey private set
+
+    fun init(plugin: JavaPlugin) {
+        EXPIRE_AT = NamespacedKey(plugin, "ghost_expires_at")
+        DEATH_WORLD = NamespacedKey(plugin, "ghost_death_world")
+        DEATH_X = NamespacedKey(plugin, "ghost_death_x")
+        DEATH_Y = NamespacedKey(plugin, "ghost_death_y")
+        DEATH_Z = NamespacedKey(plugin, "ghost_death_z")
+        RESPAWN_WORLD = NamespacedKey(plugin, "ghost_respawn_world")
+        RESPAWN_X = NamespacedKey(plugin, "ghost_respawn_x")
+        RESPAWN_Y = NamespacedKey(plugin, "ghost_respawn_y")
+        RESPAWN_Z = NamespacedKey(plugin, "ghost_respawn_z")
+    }
+}
