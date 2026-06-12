3dd90ea564374d249cc5e1ec655330442aa0a509
vuthdev <serey7vuth@gmail>
2026-06-12 13:33:41 +0700

    feat: add some help function and set PacketEvents api onLoad and terminate onDisable

--- src/main/kotlin/org/firestorm/deathproRemake/DeathproRemake.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/DeathproRemake.kt
@@ -1,21 +1,30 @@
 package org.firestorm.deathproRemake
 
+import com.github.retrooper.packetevents.PacketEvents
+import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder
 import org.bukkit.plugin.java.JavaPlugin
 import org.firestorm.deathproRemake.annotations.handler.CommandRegistry
 import org.firestorm.deathproRemake.config.DeathProConfig
+import org.firestorm.deathproRemake.eventlistener.DeathEventListener
 import org.firestorm.deathproRemake.service.GhostService
-import org.firestorm.deathproRemake.service.GraveService
 import org.firestorm.deathproRemake.storage.YamlDataStorage
 
 class DeathproRemake : JavaPlugin() {
     lateinit var deathProConfig: DeathProConfig
-        private set;
+        private set
     lateinit var ghostService: GhostService
+        private set
+
+    override fun onLoad() {
+        PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this))
+        PacketEvents.getAPI().load()
+    }
 
     override fun onEnable() {
-        saveDefaultConfig()
-        deathProConfig = DeathProConfig(this)
+        loadConfig()
+
         CommandRegistry.scan(this, DeathproRemake())
+        registerListener()
 
         val storage = YamlDataStorage(this)
         ghostService = GhostService(this)
@@ -23,6 +32,17 @@ class DeathproRemake : JavaPlugin() {
     }
 
     override fun onDisable() {
+        if (PacketEvents.getAPI() != null) {
+            PacketEvents.getAPI().terminate()
+        }
+    }
 
+    fun loadConfig() {
+        saveDefaultConfig()
+        deathProConfig = DeathProConfig(this)
+    }
+
+    fun registerListener() {
+        DeathEventListener(this).register()
     }
 }
