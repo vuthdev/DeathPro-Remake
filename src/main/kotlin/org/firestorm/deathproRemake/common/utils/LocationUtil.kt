3d402a98cad43cf8458f831160aa63545a90c569
vuthdev <serey7vuth@gmail>
2026-06-12 13:35:50 +0700

    feat: create a function to make sure player spawn on a safe location

--- src/main/kotlin/org/firestorm/deathproRemake/common/utils/LocationUtil.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/common/utils/LocationUtil.kt
@@ -1,12 +1,35 @@
 package org.firestorm.deathproRemake.common.utils
 
 import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.block.Block
 
 object LocationUtil {
     fun toReadable(loc: Location) =
         "§7(§f${loc.blockX}§7, §f${loc.blockY}§7, §f${loc.blockZ}§7)"
 
-//    fun isSafe(loc: Location): Boolean {
-//
-//    }
+    fun isSafe(loc: Location): Boolean {
+        val feet = loc.block
+        val head = feet.getRelative(0 ,1 ,0)
+        val ground = feet.getRelative(0, -1, 0)
+
+        if (!ground.type.isSolid || ground.isLiquid) {
+            return false
+        }
+
+        if (!isBlockSafeToStandIn(feet) || !isBlockSafeToStandIn(head)) {
+            return false
+        }
+
+        return true
+    }
+
+    fun isBlockSafeToStandIn(block: Block): Boolean {
+        val type: Material = block.type
+
+        return !type.isSolid
+                && type != Material.LAVA
+                && type != Material.FIRE
+                && type != Material.SOUL_FIRE
+    }
 }
