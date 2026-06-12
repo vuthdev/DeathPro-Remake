460c9e3d7d9faaba6466956ab95bb49baeb26c5b
vuthdev <serey7vuth@gmail>
2026-06-12 16:41:55 +0700

    feat: implement ghost repository and make restore ghost mode function

--- src/main/kotlin/org/firestorm/deathproRemake/service/GhostService.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/service/GhostService.kt
@@ -12,23 +12,19 @@ import org.firestorm.deathproRemake.DeathproRemake
 import org.firestorm.deathproRemake.base.BaseService
 import org.firestorm.deathproRemake.manager.GhostManager
 import org.firestorm.deathproRemake.model.GhostState
+import org.firestorm.deathproRemake.repository.GhostRepository
 import java.util.UUID
 
-class GhostService(override val plugin: DeathproRemake): BaseService(plugin) {
+class GhostService(
+    override val plugin: DeathproRemake,
+    val ghostRepository: GhostRepository
+): BaseService(plugin) {
 
     fun enterGhostMode(player: Player, deathLocation: Location) {
         val respawnLoc = player.respawnLocation
             ?: player.world.spawnLocation
 
-        player.apply {
-            gameMode = GameMode.SURVIVAL
-            allowFlight = true
-            isFlying = true
-            isInvisible = true
-            foodLevel = 20
-            saturation = 20f
-            health = player.health.plus(player.getAttribute(Attribute.MAX_HEALTH)?.value ?: 20.0)       // don't let them die again instantly
-        }
+        applyEffects(player)
 
         // Teleport to death spot so they can watch from there
         player.teleport(deathLocation.clone().add(0.0, 1.5, 0.0))
@@ -37,16 +33,17 @@ class GhostService(override val plugin: DeathproRemake): BaseService(plugin) {
         val countdownSeconds = config.ghost.duration  // e.g. 10
         val task = startCountdown(player, countdownSeconds)
 
-        GhostManager.add(
-            GhostState(
-                playerUuid = player.uniqueId,
-                playerName = player.name,
-                deathLocation = deathLocation,
-                respawnLocation = respawnLoc,
-                taskId = task.taskId,
-                expiresAt = 5,
-            )
+         val ghostState = GhostState(
+            playerUuid = player.uniqueId,
+            playerName = player.name,
+            deathLocation = deathLocation,
+            respawnLocation = respawnLoc,
+            taskId = task.taskId,
+            expiresAt = 5,
         )
+
+        GhostManager.add(ghostState)
+        ghostRepository.save(player, ghostState)
 
         val title = Component.text("§cYou died!")
         val subTitle = Component.text("§7Respawning in §f${countdownSeconds}s §7— press §fSHIFT §7to respawn now")
@@ -55,13 +52,45 @@ class GhostService(override val plugin: DeathproRemake): BaseService(plugin) {
         player.showTitle(sendTitle)
     }
 
+    fun restoreGhostMode(player: Player) {
+        val state = ghostRepository.load(player)
+
+        when {
+            state.isExpired -> {
+                ghostRepository.clear(player)
+                logger.info("Clear ghost state for ${player.name}")
+            }
+            else -> {
+                applyEffects(player)
+
+                player.teleport(state.deathLocation)
+
+                val remaining = state.remainingSeconds.toInt()
+                val task = startCountdown(player, remaining)
+                GhostManager.update(player.uniqueId, { state ->
+                    state.copy(taskId = task.taskId)
+                })
+
+                player.showTitle(
+                    Title.title(
+                        Component.text("§cStill dead!"),
+                        Component.text("§7Respawning in §f${remaining}s"),
+                        10, 60, 20
+                    )
+                )
+                logger.info("Restored ghost mode for ${player.name}, ${remaining}s remaining.")
+            }
+        }
+    }
+
     fun exitGhostMode(uuid: UUID) {
         val state = GhostManager.get(uuid) ?: return
-        val player = Bukkit.getPlayer(state.playerUuid)!!
+        val player = Bukkit.getPlayer(state.playerUuid) ?: return
 
         // Cancel countdown task
         scheduler.cancelTask(state.taskId)
         GhostManager.remove(uuid)
+        ghostRepository.clear(player)
 
         if (player.isOp) {
             player.apply {
@@ -101,5 +130,17 @@ class GhostService(override val plugin: DeathproRemake): BaseService(plugin) {
             player.sendActionBar(Component.text("§7Respawning in §f${remaining}s §7— Shift to respawn now"))
             remaining--
         }, 0L, 20L) // every 20 ticks = 1 second
+    }
+
+    fun applyEffects(player: Player) {
+        player.apply {
+            gameMode = GameMode.SURVIVAL
+            allowFlight = true
+            isFlying = true
+            isInvisible = true
+            foodLevel = 20
+            saturation = 20f
+            health = player.health.plus(player.getAttribute(Attribute.MAX_HEALTH)?.value ?: 20.0)       // don't let them die again instantly
+        }
     }
 }
