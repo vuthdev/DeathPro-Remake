68d4111ae6b016cc2e458d4aad160eae27d41994
vuthdev <serey7vuth@gmail.com>
2026-06-14 00:42:10 +0700

    feat: instant respawn, save location and override respawn location

--- src/main/kotlin/org/firestorm/deathproRemake/eventlistener/OnRespawnListener.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/eventlistener/OnRespawnListener.kt
@@ -1,4 +1,28 @@
 package org.firestorm.deathproRemake.eventlistener
 
-class OnRespawnListener {
+import org.bukkit.Bukkit
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.player.PlayerRespawnEvent
+import org.firestorm.deathproRemake.DeathproRemake
+import org.firestorm.deathproRemake.base.BaseListener
+import org.firestorm.deathproRemake.manager.GhostPostRespawnPending
+import org.firestorm.deathproRemake.manager.GhostRespawnPending
+
+class OnRespawnListener(private val p: DeathproRemake): BaseListener(p) {
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    fun onRespawn(e: PlayerRespawnEvent) {
+        val player = e.player
+
+        if (GhostRespawnPending.isResolving(player.uniqueId)) return
+
+        val data = GhostRespawnPending.get(player.uniqueId) ?: return
+        GhostRespawnPending.remove(player.uniqueId)
+
+        // override respawn location
+        e.respawnLocation = data.deadLocation
+
+        GhostPostRespawnPending.add(player.uniqueId, data)
+    }
 }
