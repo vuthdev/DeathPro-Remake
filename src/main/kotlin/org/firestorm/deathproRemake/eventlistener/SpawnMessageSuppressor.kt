3cef1631b34b4a4e7feffe1fcb93fbc06e703967
vuthdev <serey7vuth@gmail.com>
2026-06-14 00:38:56 +0700

    fix: send packet to fix no home message

--- src/main/kotlin/org/firestorm/deathproRemake/eventlistener/SpawnMessageSuppressor.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/eventlistener/SpawnMessageSuppressor.kt
@@ -1,4 +1,23 @@
 package org.firestorm.deathproRemake.eventlistener
 
-class SpawnMessageSuppressor {
+import com.github.retrooper.packetevents.event.PacketListenerAbstract
+import com.github.retrooper.packetevents.event.PacketSendEvent
+import com.github.retrooper.packetevents.protocol.packettype.PacketType
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSystemChatMessage
+import org.firestorm.deathproRemake.base.BasePacketListener
+
+class SpawnMessageSuppressor: BasePacketListener() {
+    override fun onPacketSend(event: PacketSendEvent) {
+        if (event.packetType != PacketType.Play.Server.SYSTEM_CHAT_MESSAGE) return
+
+        val packet = WrapperPlayServerSystemChatMessage(event)
+
+        // the vanilla key is "block.minecraft.spawn.not_valid"
+        val message = packet.message.toString()
+        if (message.contains("spawn.not_valid") ||
+            message.contains("no home bed") ||
+            message.contains("You have no home bed")) {
+            event.isCancelled = true
+        }
+    }
 }
