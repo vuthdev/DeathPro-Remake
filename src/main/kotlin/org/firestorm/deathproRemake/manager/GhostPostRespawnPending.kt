934f650d6c84545323e2ff248235375562479d41
vuthdev <serey7vuth@gmail.com>
2026-06-14 00:36:51 +0700

    feat: set pending before post respawn

--- src/main/kotlin/org/firestorm/deathproRemake/manager/GhostPostRespawnPending.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/manager/GhostPostRespawnPending.kt
@@ -1,4 +1,15 @@
 package org.firestorm.deathproRemake.manager
 
+import org.firestorm.deathproRemake.dto.GhostLocationData
+import java.util.UUID
+import java.util.concurrent.ConcurrentHashMap
+
 object GhostPostRespawnPending {
+    private val pending = ConcurrentHashMap<UUID, GhostLocationData>()
+
+    fun add(uuid: UUID, data: GhostLocationData) {
+        pending[uuid] = data
+    }
+    fun remove(uuid: UUID) = pending.remove(uuid)
+    fun get(uuid: UUID) = pending[uuid]
 }
