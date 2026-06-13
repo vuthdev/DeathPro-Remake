91d6dc340c79077c789100048928029d06103b6e
vuthdev <serey7vuth@gmail.com>
2026-06-14 00:33:08 +0700

    feat: add respawn location to GhostState data

--- src/main/kotlin/org/firestorm/deathproRemake/model/GhostState.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/model/GhostState.kt
@@ -1,15 +1,16 @@
 package org.firestorm.deathproRemake.model
 
 import org.bukkit.Location
+import org.firestorm.deathproRemake.common.enums.CorpseStateEnum
 import java.util.UUID
 
 data class GhostState(
-    val playerUuid: UUID,
-    val playerName: String,
-    val deathLocation: Location,
-    val respawnLocation: Location,
-    val expiredAt: Long,
-    val taskId: Int = -1,
+    var playerUuid: UUID,
+    var playerName: String,
+    var deathLocation: Location,
+    var respawnLocation: Location,
+    var expiredAt: Long,
+    var taskId: Int = -1,
 ) {
     val remainingSeconds: Long
         get() = ((expiredAt - System.currentTimeMillis()) / 1000).coerceAtLeast(0)
