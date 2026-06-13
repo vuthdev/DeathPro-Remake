f28fcf4fc537eb40a09c011e6355b0cb12c6b8f8
vuthdev <serey7vuth@gmail.com>
2026-06-14 00:41:18 +0700

    feat: apply effect and apply ghost mode

--- src/main/kotlin/org/firestorm/deathproRemake/eventlistener/OnPostRespawnListener.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/eventlistener/OnPostRespawnListener.kt
@@ -1,4 +1,19 @@
 package org.firestorm.deathproRemake.eventlistener
 
-class OnPostRespawnListener {
+import com.destroystokyo.paper.event.player.PlayerPostRespawnEvent
+import org.bukkit.event.EventHandler
+import org.firestorm.deathproRemake.DeathproRemake
+import org.firestorm.deathproRemake.base.BaseListener
+import org.firestorm.deathproRemake.manager.GhostPostRespawnPending
+
+class OnPostRespawnListener(private val p: DeathproRemake) : BaseListener(p) {
+
+    @EventHandler
+    fun onPostRespawn(e: PlayerPostRespawnEvent) {
+        val player = e.player
+        val data = GhostPostRespawnPending.get(player.uniqueId) ?: return
+        GhostPostRespawnPending.remove(player.uniqueId)
+
+        p.ghostService.enterGhostMode(player, data.deadLocation, data.respawnLocation)
+    }
 }
