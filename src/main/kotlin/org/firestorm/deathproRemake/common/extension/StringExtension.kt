6bb68523ba780e6099070fb7ad19b7ee5b5fba65
vuthdev <serey7vuth@gmail>
2026-06-13 11:40:17 +0700

    feat: add color extension function support both mini message and legacy color codes

--- src/main/kotlin/org/firestorm/deathproRemake/common/extension/StringExtension.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/common/extension/StringExtension.kt
@@ -1,2 +1,15 @@
 package org.firestorm.deathproRemake.common.extension
 
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
+
+fun String.color(): Component {
+    val hasLegacy = this.contains(Regex("&[0-9a-fk-orA-FK-ORxX]"))
+
+    return if (hasLegacy) {
+        LegacyComponentSerializer.legacyAmpersand().deserialize(this)
+    } else {
+        MiniMessage.miniMessage().deserialize(this)
+    }
+}
