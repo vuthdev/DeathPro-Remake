bb08d0e5e8aacd904512c4fd6ca445b2e887aacb
vuthdev <serey7vuth@gmail.com>
2026-06-16 14:54:08 +0700

    feat: utility for creeate user profile

--- src/main/kotlin/org/firestorm/deathproRemake/common/utils/UserProfileUtil.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/common/utils/UserProfileUtil.kt
@@ -1,14 +1,24 @@
 package org.firestorm.deathproRemake.common.utils
 
+import com.github.retrooper.packetevents.protocol.player.TextureProperty
 import com.github.retrooper.packetevents.protocol.player.UserProfile
+import io.github.retrooper.packetevents.util.SpigotReflectionUtil
 import org.bukkit.entity.Player
+import org.firestorm.deathproRemake.common.constants.BaseConstants
+import org.firestorm.deathproRemake.common.extension.clogger
 import java.util.UUID
 
 object UserProfileUtil {
     fun createProfile(player: Player): UserProfile {
+        val playerProfile = player.playerProfile
+        val properties = playerProfile.properties
+            .filter { it.name == "textures" }
+            .map { TextureProperty(it.name, it.value, it.signature) }
+
         return UserProfile(
             UUID.randomUUID(),
             player.name,
+            properties
         )
     }
 }
