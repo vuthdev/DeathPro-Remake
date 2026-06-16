9d906ac3e96cc8f4cd60db5018a1fe0f2b15e9dc
vuthdev <serey7vuth@gmail.com>
2026-06-16 15:05:04 +0700

    feat: create an extension to convert to equipment list

--- src/main/kotlin/org/firestorm/deathproRemake/common/extension/PlayerEquipmentExtension.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/common/extension/PlayerEquipmentExtension.kt
@@ -1,2 +1,27 @@
 package org.firestorm.deathproRemake.common.extension
 
+import com.github.retrooper.packetevents.protocol.player.Equipment
+import com.github.retrooper.packetevents.protocol.player.EquipmentSlot
+import io.github.retrooper.packetevents.util.SpigotConversionUtil
+import org.bukkit.inventory.ItemStack
+import org.firestorm.deathproRemake.model.PlayerEquipment
+
+fun PlayerEquipment.fromPlayerEquipment(): List<Equipment> {
+    val equipmentList = mutableListOf<Equipment>()
+
+    val addSlot = { slot: EquipmentSlot, item: ItemStack? ->
+        if (item != null && !item.type.isAir) {
+            val packetItem = SpigotConversionUtil.fromBukkitItemStack(item)
+            equipmentList.add(Equipment(slot, packetItem))
+        }
+    }
+
+    addSlot(EquipmentSlot.MAIN_HAND, this.mainHand)
+    addSlot(EquipmentSlot.OFF_HAND, this.offHand)
+    addSlot(EquipmentSlot.HELMET, this.helmet)
+    addSlot(EquipmentSlot.CHEST_PLATE, this.chestplate)
+    addSlot(EquipmentSlot.LEGGINGS, this.leggings)
+    addSlot(EquipmentSlot.BOOTS, this.boots)
+
+    return equipmentList
+}
