7f7d049d10ee06bc1308dd8888ed84bae8bcbc64
vuthdev <serey7vuth@gmail.com>
2026-06-16 15:06:34 +0700

    feat: add data class for npc equipment

--- src/main/kotlin/org/firestorm/deathproRemake/model/PlayerEquipment.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/model/PlayerEquipment.kt
@@ -1,3 +1,12 @@
 package org.firestorm.deathproRemake.model
 
-data class PlayerEquipment()
+import org.bukkit.inventory.ItemStack
+
+data class PlayerEquipment(
+    var mainHand: ItemStack?,
+    var offHand: ItemStack?,
+    var helmet: ItemStack?,
+    var chestplate: ItemStack?,
+    var leggings: ItemStack?,
+    var boots: ItemStack?
+)
