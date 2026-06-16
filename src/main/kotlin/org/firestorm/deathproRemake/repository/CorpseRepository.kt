614633c11eca2b6c1e5f8b4fe89d17c5554b8f1e
vuthdev <serey7vuth@gmail.com>
2026-06-16 14:55:19 +0700

    feat: CRUD function for DB

--- src/main/kotlin/org/firestorm/deathproRemake/repository/CorpseRepository.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/repository/CorpseRepository.kt
@@ -1,4 +1,84 @@
 package org.firestorm.deathproRemake.repository
 
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.firestorm.deathproRemake.common.utils.EquipmentSerializer
+import org.firestorm.deathproRemake.model.CorpseState
+import org.firestorm.deathproRemake.model.NpcSkinData
+import org.firestorm.deathproRemake.storage.database.table.CorpseTable
+import org.jetbrains.exposed.v1.core.ResultRow
+import org.jetbrains.exposed.v1.core.eq
+import org.jetbrains.exposed.v1.core.greater
+import org.jetbrains.exposed.v1.core.lessEq
+import org.jetbrains.exposed.v1.jdbc.deleteWhere
+import org.jetbrains.exposed.v1.jdbc.selectAll
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import org.jetbrains.exposed.v1.jdbc.upsert
+
 object CorpseRepository {
+
+    fun insert(state: CorpseState) = transaction {
+        CorpseTable.upsert {
+            it[corpseId] = state.corpseId
+            it[playerUuid] = state.playerUuid
+            it[playerName] = state.playerName
+            it[world] = state.location.world?.name ?: "world"
+            it[x] = state.location.x
+            it[y] = state.location.y
+            it[z] = state.location.z
+            it[skinTexture] = state.skin.skinTexture
+            it[skinSignature] = state.skin.skinSignature
+            it[expiredAt] = state.expiredAt
+            it[spawnedAt] = state.spawnedAt
+
+            // convert to json
+            it[equipmentJson] = EquipmentSerializer.toJson(state.equipment)
+        }
+    }
+
+    fun delete(corpseId: Int) = transaction {
+        CorpseTable.deleteWhere {
+            CorpseTable.corpseId eq corpseId
+        }
+    }
+
+    fun deleteExpired() = transaction {
+        CorpseTable.deleteWhere {
+            expiredAt lessEq System.currentTimeMillis()
+        }
+    }
+
+    fun loadActive(): List<CorpseState> = transaction {
+        CorpseTable.selectAll()
+            .where { CorpseTable.expiredAt greater System.currentTimeMillis() }
+            .mapNotNull { runCatching { it.toCorpseState() }.getOrNull() }
+    }
+
+    /*
+     ==============================
+     ====== PRIVATE FUNCTION ======
+     ==============================
+     */
+
+    private fun ResultRow.toCorpseState(): CorpseState? {
+        val world = Bukkit.getWorld(this[CorpseTable.world]) ?: return null
+        return CorpseState(
+            corpseId = this[CorpseTable.corpseId],
+            playerName = this[CorpseTable.playerName],
+            playerUuid = this[CorpseTable.playerUuid],
+            location = Location(
+                world,
+                this[CorpseTable.x],
+                this[CorpseTable.y],
+                this[CorpseTable.z]
+            ),
+            skin = NpcSkinData(
+                skinTexture = this[CorpseTable.skinTexture],
+                skinSignature = this[CorpseTable.skinSignature]
+            ),
+            expiredAt = this[CorpseTable.expiredAt],
+            spawnedAt = this[CorpseTable.spawnedAt],
+            equipment = EquipmentSerializer.fromJson(this[CorpseTable.equipmentJson])
+        )
+    }
 }
