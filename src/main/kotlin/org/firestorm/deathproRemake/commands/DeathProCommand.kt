d36b3481423ed443c7b62cd9fc07beba0ed5ed43
vuthdev <serey7vuth@gmail.com>
2026-06-17 09:06:37 +0700

    fix: commands message

--- src/main/kotlin/org/firestorm/deathproRemake/commands/DeathProCommand.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/commands/DeathProCommand.kt
@@ -21,23 +21,35 @@ class DeathProCommand(
 
     @SubCommand
     fun default(sender: CommandSender) {
-        sender.sendMessage("/deathpro - list all command")
+        sendHelpMenu(sender)
     }
 
     @SubCommand(name = "help", permission = "deathpro.help")
     fun help(sender: CommandSender) {
-        sender.sendMessage("/help - list all command")
+        sendHelpMenu(sender)
     }
 
     @SubCommand(name = "spawncorpse", playerOnly = true, permission = "deathpro.spawncorpse")
     fun spawnCorpse(sender: CommandSender) {
         val player = sender as? Player ?: return
+        player.sendMessage("${p.messageConfig.rawPrefix()}spawned successfully!")
         corpseService.spawnCorpse(player)
     }
 
     @SubCommand(name = "reload", permission = "deathpro.reload")
     fun reload(sender: CommandSender) {
         p.reloadAllConfig()
-        sender.sendMessage("${p.messageConfig.rawPrefix()} &areload config successfully".color())
+        sender.sendMessage("${p.messageConfig.rawPrefix()}&areload config successfully".color())
+    }
+
+    fun sendHelpMenu(sender: CommandSender) {
+        sender.sendMessage("&8&l&m---------------------------------------------".color())
+        sender.sendMessage("&c&lDeathPro &7- Command Help Menu".color())
+        sender.sendMessage("&8&l&m---------------------------------------------".color())
+        sender.sendMessage("&7» &c/deathpro &8- &7main command.".color())
+        sender.sendMessage("&7» &c/deathpro help &8- &7Show this help menu.".color())
+        sender.sendMessage("&7» &c/deathpro reload &8- &7Reload the config.yml safely.".color())
+        sender.sendMessage("&7» &c/deathpro spawncorpse &8- &7Test-spawn a corpse at your spot.".color())
+        sender.sendMessage("&8&l&m---------------------------------------------".color())
     }
 }
