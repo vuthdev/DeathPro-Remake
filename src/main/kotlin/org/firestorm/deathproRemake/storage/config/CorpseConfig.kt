01c0fe4ba6cdaa485ed81331b2f475887ab6c300
vuthdev <serey7vuth@gmail.com>
2026-06-17 16:56:39 +0700

    feat: add pose to config for easier access

--- src/main/kotlin/org/firestorm/deathproRemake/storage/config/CorpseConfig.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/storage/config/CorpseConfig.kt
@@ -3,6 +3,6 @@ package org.firestorm.deathproRemake.storage.config
 import org.bukkit.configuration.file.FileConfiguration
 
 class CorpseConfig(config: FileConfiguration) : ConfigSection(config, "corpse") {
-    val enabled get() = boolean("enabled", true)
+    val pose get() = string("pose", "SLEEPING")
     val duration get() = int("duration", 300)
 }
