3ab88a31d689dbdc52842423ff5e9f0baf051776
vuthdev <serey7vuth@gmail.com>
2026-06-17 16:55:54 +0700

    feat: add onChat listener for allow-chat config

--- src/main/kotlin/org/firestorm/deathproRemake/eventlistener/OnChatListener.kt
+++ src/main/kotlin/org/firestorm/deathproRemake/eventlistener/OnChatListener.kt
@@ -1,4 +1,19 @@
 package org.firestorm.deathproRemake.eventlistener
 
-class OnChatListener {
+import io.papermc.paper.event.player.AsyncChatEvent
+import org.bukkit.event.EventHandler
+import org.firestorm.deathproRemake.DeathproRemake
+import org.firestorm.deathproRemake.base.BaseListener
+import org.firestorm.deathproRemake.common.extension.isGhost
+
+class OnChatListener(private val p: DeathproRemake): BaseListener(p) {
+
+    @EventHandler
+    fun onChat(e: AsyncChatEvent) {
+        val player = e.player
+        if (!player.isGhost() && !config.ghost.allowChat) return
+
+        e.isCancelled = true
+        player.sendMessage(messageConfig.ghostCantInteract)
+    }
 }
