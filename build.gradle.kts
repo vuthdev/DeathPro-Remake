3350b837242d78059cc566423de6c515c59aaf64
vuthdev <serey7vuth@gmail.com>
2026-06-19 09:38:05 +0700

    build jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation("com.github.retrooper:packetevents-spigot:2.12.2")
     implementation("org.jetbrains.exposed:exposed-core:1.3.0")
     implementation("org.jetbrains.exposed:exposed-jdbc:1.3.0")
+    implementation("org.jetbrains.exposed:exposed-migration-jdbc:1.3.0")
 
     implementation("org.xerial:sqlite-jdbc:3.53.2.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
